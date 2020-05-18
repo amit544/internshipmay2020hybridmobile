24d5092ada9877d824669c83f1aa4c983ddce6bb
amitsrivastava <amitapm544@gmail.com>
2020-05-18 10:54:36 +0530

    Product
    Product of two no

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function product(){
+    num1 = document.getElementById("num1").value;
+     num2 = document.getElementById("num2").value;
+        var result=num1*num2;
+            alert(result)
+}
